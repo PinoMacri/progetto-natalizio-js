31b90b74f7d23fdd3bc98b9a88e73ec6e7395fba
PinoMacri <pinonegozio1@outlook.it>
2022-12-28 20:22:26 +0100

    Finish Header

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 //^ --------------------- HEADER ---------------------*/
+//**----- Prendo Elementi Header -----*/
 const burgerMenu = document.getElementById("burgerMenu")
 const officialMenu = document.getElementById("officialMenu")
 const closeMenu = document.getElementById("closeMenu")
+//**----- Burger Menù -----*/
 burgerMenu.addEventListener("click", function () {
     burgerMenu.classList.add("d-none");
     closeMenu.classList.remove("d-none")
-    
+    officialMenu.classList.remove("d-none", "align-items-center");
+    officialMenu.classList.add("flex-column", "align-items-end");
 })
+//**----- Close Menù -----*/
 closeMenu.addEventListener("click", function () {
     burgerMenu.classList.remove("d-none");
     closeMenu.classList.add("d-none")
+    officialMenu.classList.add("d-none");
 })
+//~-----------------------------------------------------*/
