ce7ed0ed744d586dcb3c1dbcfe6d77ef0ea19493
PinoMacri <pinonegozio1@outlook.it>
2022-12-29 02:34:51 +0100

    Sostituzione immagini statiche a dinamiche JS

--- js/script.js
+++ js/script.js
@@ -18,7 +18,25 @@ closeMenu.addEventListener("click", function () {
 })
 //~-----------------------------------------------------*/
 //^ ----------------------- MAIN -----------------------*/
-
+const carousel = document.getElementById("carouselExampleAutoplaying")
+carousel.innerHTML =
+ `  <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="img/digital-art-warrior-knight-horse-shield-Sun-1512243-wallhere.com.jpg" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/4649701.jpg" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/1237368.jpg" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/1237391.jpg" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/3976786.jpg" alt="...">
+                </div>
+    </div>`
 
 
 
